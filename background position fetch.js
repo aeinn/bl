bf0c530ad5892a3641485705ff487b5b4cbfee79
aii-yin <46767190+aeinn@users.noreply.github.com>
2022-09-08 11:26:24 +0800

    Update background position fetch.js

--- background position fetch.js
+++ background position fetch.js
@@ -1,7 +1,7 @@
 var i = 0
 var wok = "im a service worker that has been active for: "
 
-setTimeout(function(){
+setInterval(function(){
     i++
     postMessage(wok + i*5 + " seconds")
 }, 1000 * 5)
